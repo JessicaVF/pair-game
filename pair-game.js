93c328f25c6a032ce90e7077b88c2f3a8bcaefa6
JessicaVF <villarjessica@hotmail.com>
2021-02-08 18:07:18 +0100

    basic logic to assign value to cells

--- pair-game.js
+++ pair-game.js
@@ -1,12 +1,16 @@
-
-
 const tdClickArea = document.querySelectorAll("td");
 let p1 = [];
 let p2 = [];
-let cartes = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6"];
+let cartes = ["1", "1", "2", "2", "3", "3", "4", "4"];
 cartes = shuffle(cartes);
 let p1turn = true;
 let p2turn = false;
+
+//dummy system to test: 
+let cartas = [["1", "1", "2", "2"], ["3", "3", "4", "4"]];
+cartas = [shuffle(cartas[0]), shuffle(cartas[1])];
+console.log(cartas);
+//
 
 function shuffle(a) {
     var j, x, i;
@@ -22,11 +26,11 @@ function shuffle(a) {
 tdClickArea.forEach(item => {
     item.addEventListener('click', e => {
         let btnArea = e.target;
-        let z = btnArea.cellIndex;
-        console.log("c index is: " + z);
-        //let indexTD;
-
-        btnArea.innerHTML = cartes[z];
+        let indexCell = btnArea.cellIndex;
+        let indexRow = btnArea.parentElement.rowIndex;
+        console.log(indexCell);
+        console.log(indexRow);
+        btnArea.innerHTML = cartas[indexRow][indexCell];
         //changeTurn();
     })
 });
