cbfb0d23b4f17a7d01a061171633853baf39ac97
JessicaVF <villarjessica@hotmail.com>
2021-02-08 12:34:57 +0100

    starting basic logic

---
+++ pair-game.js
@@ -0,0 +1,18 @@
+const tdClickArea = document.querySelectorAll("td");
+
+
+let p1 = [];
+let p2 = [];
+let cartes = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+cartes = shuffle(cartes);
+let p1turn = true;
+let p2turn = false;
+
+tdClickArea.forEach(item => {
+    item.addEventListener('click', e => {
+        let btnArea = e.target;
+        btnArea.innerHTML =
+        //changeTurn();
+        }
+    })
+});
